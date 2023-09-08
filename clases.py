680e39cd74b71a08c22cbce0df903b517d3ae961
ranitadelamatrix <gastontrejo9099@hotmail.com>
2023-09-07 21:10:23 -0300

    "terminado las clases"

--- clases.py
+++ clases.py
@@ -1,4 +1,5 @@
-import re 
+import re
+import datetime
 
 from validate_email_address import validate_email
 
@@ -11,7 +12,7 @@ A su vez, cada curso pertenece a alguna de las
 siguientes categorías (Inicial, Intermedio, Avanzado)<---metodos."""
 
 
-
+listadeclases = []
 class Curos1():
     #de esta forma creamos los atributos de inicializacion
        
@@ -26,10 +27,10 @@ class Curos1():
                 self.duracion_meses = duracion_meses
                 self.estado = estado
                 self.categoria = categoria
-                self.listadeclases = []
+                
 
        def __str__(self) -> str:
-          return f"Hola bienvenido al curso el mismo comieza el dia {self.fecha}\nnuestro curso se llama {self.titulo}\nen el veremos {self.descripcion}\nnuestro objetivo es que {self.objetivos}\nnuestro programa es {self.programa}\nel curso tiene un valor de {self.costo}\ntu foto de perfil es {self.foto}\nel mismo tiene una duracion de {self.duracion_meses} meses\nel curso se encuentra {self.estado}"
+          return f"Hola bienvenido al curso de {self.titulo} \nel mismo comieza el dia {self.fecha}\nen el veremos {self.descripcion}\nnuestro objetivo es que {self.objetivos}\nnuestro programa es {self.programa}\nel curso tiene un valor de {self.costo}\ntu foto de perfil es {self.foto}\nel mismo tiene una duracion de {self.duracion_meses} meses\nel curso se encuentra {self.estado}"
     
        def estado_del_curso(self): #metodo disponibilidad
                 if self.estado == "disponible":
@@ -43,15 +44,20 @@ class Curos1():
                       return ("intermedio")
                else:
                       return ("avanzado")
-      
+       def agregarClases(self, a):
+              listadeclases.append(a)
+              print("se agrego una clase a tu curso")
+       
        def verclases(self):
-              for i in self.listadeclases:
-                     print(f" fecha {i[0]}, el titulo {i[1]}, contenido {i[2]}, la direccion del drive {i[3]}")
+              for i in listadeclases:
+                     print(i)
         
 """Por otro lado, los cursos contienen un conjunto de clases,
 en donde por cada clase se debe
 mostrar la fecha, título, contenido, URLDrive."""
-curiiii = Curos1("22.55.88", "uno", "probando", "aprender", "2", 4000, "foto.png", 8, "disponible", "inicial")
+
+
+
 
 class Clases():
         
@@ -60,10 +66,17 @@ class Clases():
               self.titulo = titulo
               self.contenido = contenido
               self.URLDrive = URLDrive
-          
+       def __str__(self)->str:
+              return f"Fecha de la clase {self.fecha}, Titulo {self.titulo}, El contenido {self.contenido}, Link del Drive {self.URLDrive}"
+matematica = Clases("23.85.99", "cmica", "matriz", "wwwww")
+lengua =   Clases("23.85.99", "literatura", "verbo", "wdddwwww")
        
+curiiii = Curos1("22.55.88", "uno", "probando", "aprender", "2", 4000, "foto.png", 8, "disponible", 1)
+quiimica = Curos1("22.55.88", "quimicaaa", "probando", "aprender", "2", 4000, "foto.png", 8, "disponible", 1)
 
-v = Clases("23.85.99", "cmica", "matriz", "wwwww")
+
+
+
 
 
 
@@ -92,6 +105,7 @@ class Docente():
 docent = Docente("terjo", "gaston", 33669317, "02/06/1988", "catamarca 2660", "villa maria", 5900, "cordoba", 385417268, "gastontrejo9099@hotmail.com")
 
 
+
 """Por otra parte, los interesados en inscribirse a un curso deberán crearse una cuenta de usuario
 final, donde deberán registrar su datos personales (nombre, apellido, dni, dirección, fecha
 nacimiento, dirección, localidad, código postal, provincia, teléfono celular, email) además de
@@ -112,6 +126,17 @@ class RegistrarUsuario():
               self.provincia = provincia
               self.celular = celular
               self.email = email
+              self.listaDeCursos = []
+
+       def get_apellido(self):
+              return self.apellido
+       def get_nombre(self):
+              return self.nombre
+       def get_dni(self):
+              return self.dni
+       def get_email(self):
+              return self.email
+
 
        def validacion(self):
                             
@@ -138,14 +163,33 @@ class RegistrarUsuario():
                                    print("corrabore sus datos")
                      else:
                             print("correo no valido")
+       def agregarCurso(self, a):
+              self.listaDeCursos.append(a)
+              print("se agrego curso", a.titulo)
+
               
-              
 
 
 
-a = RegistrarUsuario("gaston", "trejo", 336699, "vatamar", "12/15/15", "villa maria", 5900, "cordoba", 3854172687, "gastontrejo9099@hotmail.com")
+gas = RegistrarUsuario("gaston", "trejo", 336699, "vatamar", "12/15/15", "villa maria", 5900, "cordoba", 3854172687, "gastontrejo9099@hotmail.com")
 
 
+
+class Administrador():
+       def __init__(self, apellido, nombre, dni, nacimiento, direccion, localidad, codigo_postal, provincia, celular, email):
+              self.apellido = apellido
+              self.nombre = nombre
+              self.dni = dni
+              self.nacimiento = nacimiento
+              self.direccion = direccion
+              self.localidad = localidad
+              self.codigo_postal = codigo_postal
+              self.provincia = provincia
+              self.celular = celular
+              self.email =email
+
+       def __str__(self) -> str:
+              return f"DATOS DEL ADMINISTRADOR\nApellido {self.apellido}\nNombre {self.nombre}\nDNI {self.dni}\nNacimiento {self.nacimiento}\nDireccion {self.direccion}\nLocalidad {self.localidad}\nCodigo Postal {self.codigo_postal}\nProvincia {self.provincia}\ntelefono celular {self.celular}\nEmail {self.email}"
 
 
 """Los usuarios públicos registrados pueden inscribirse a uno o más cursos. Además, el sitio
@@ -154,26 +198,117 @@ interacción con el sistema: Administrador, Docente. Los usuarios también deben
 asociado un estado (Activo / Inactivo)."""
 
 class Usuarios():
-       def __init__(self, usuarios, cursos)->None:
-              self.usuarios = usuarios
-              self.cursos = cursos
-       def inscripcionCurso(self):
-              self.listaCursos = []
-              if self.usuarios.validacion() == True:
-                     self.listaCursos.append(self.cursos)
-                     print("se agrego un curso")
-              else:
-                     print("no se aegrego")
+       
+       def rol(self, a):
+              rol = int(input("si desea incribirse como docente presione 1 ""\n""si desesa incribirse como administrador presione 2 "))
+              if rol == 1:
+                     
+                     print("Bienvenido profe a continuacion corrboraremos sus datos para crear su usario al curso")
+                     print(a)
+                     correcion = int(input("Si sus datos son correcto presione 1, si no presione 2 "))
+                     if correcion == 1:
+                            generar = input("Genere su contraseña, recuerde que ambas deben coincidir, presione enter para continuar")
+                            while True:
+                                   clave1 = input("Ingrese una clave por favor: ")
+                                   clave2=input ("Repita la clave para confirmarla: ")
+                                   if clave1==clave2:
+                                          print(f"Se a enviado un correo automatico a {a.email} para su validacion")
+                                          print("Una vez validada ya podra interactuar con el curos")
+                                          break
+                                   else:
+                                          print("no coinciden las contraseñas vuelva a intentar ")
+              elif rol == 2:
+                     print("Bienvenido como administrado, a continuacion corraboraremos sus datos para crear su usasrio")
+                     print(a)
+                     correcion = int(input("Si sus datos son correcto presione 1, si presione 2 "))
+                     if correcion == 1:
+                            generar = input("Genere su contraseña, recuerde que ambas deben coincidir, presione enter para continuar")
+                            while True:
+                                   clave1 = input("Ingrese una clave por favor: ")
+                                   clave2=input ("Repita la clave para confirmarla: ")
+                                   if clave1==clave2:
+                                          print(f"Se a enviado un correo automatico a {a.email} para su validacion")
+                                          print("Una vez validada ya podra interactuar con el curos")
+                                          break
+                                   else:
+                                          print("no coinciden las contraseñas vuelva a intentar ")
 
-abb = Usuarios(a, curiiii)
-abb.inscripcionCurso()
+abb = Usuarios()
+
+
 
 """Por último, los usuarios finales podrán agregar uno o más cursos en un carrito de compras,
-donde se deberá visualizar: Foto, título del curso, duración, costo. Una vez confirmados los
-ítems del carrito, deberá seleccionar el medio de pago, los cuales pueden ser: Tarjeta de
-crédito/débito y transferencia bancaria; a fin de tener más datos acerca de los medios de
-pago, deberán registrarse los datos básicos de tarjetas y transferencias). Al confirmar la
+donde se deberá visualizar: Foto, título del curso, duración, costo.
+Una vez confirmados los ítems del carrito, 
+
+deberá seleccionar el medio de pago, los cuales pueden ser: Tarjeta de
+crédito/débito y transferencia bancaria; 
+
+a fin de tener más datos acerca de los medios de
+pago, deberán registrarse los datos básicos de tarjetas y transferencias).
+Al confirmar la
 compra, se deberá registrar además la fecha de compra, el usuario que realiza la compra y el
-monto total. Dada la consigna anterior, se pide: identificar, analizar y documentar los
-requisitos según el formato de historias de usuario. Para la creación de las historias, hacer uso
-del repositorio Github, a través de creación de issues."""
+monto total."""
+
+listaDeCurso = []
+class Carrito():
+             
+              def __init__(self, a):
+                     self.curso = a
+                     print(f"-----Usted eligio el curso de: \n{a.titulo}\nFoto{a.foto}\nDuracion {a.duracion_meses}\nCon un costo de {a.costo} pesos")
+                     res = input(f"desea agregar {a.titulo} al carrito de compras? digite si ")
+                     if res == "si":
+                            listaDeCurso.append(a)
+                            print("se agrego ", a.titulo)
+              def verCursos(self):
+                    print("Sus cursos")
+                    for i in listaDeCurso:
+                           print(f"Curso de {i.titulo}")
+              
+class Pagos():
+       def __init__(self)->None:
+                     self.resultado = 0
+                     for i in listaDeCurso:
+                            self.resultado += i.costo
+                     print(f"el total a pagar es de {self.resultado}")
+       def mediosPagos(self, a):
+              pagos = int(input("Si desea pagar con tarjeta de credito digite 1\nsi desea pagar con tarjeta de debito digite 2\nsi desea pagar con tranferencia bancaria digite 3"))
+              if pagos == 1:
+                     recargo = self.resultado * 10 / 100
+                     total = recargo + self.resultado
+                     print("Usted a elegido tarjeta de credito, tiene un recargo de el 10 por ciento el total es: ", total)
+                     print("")
+                     nombreusuario = a.nombre
+                     numeroTarjeta = int(input("Ingrese el numero de tarjeta por favor"))
+                     nombreTitular = input("Ingrese el nombre que figura en al tarjeta")
+                     codigo = int(input("ingrese el codigo de seguridad que figura al dorso de la tarjeta"))
+                     cantidad = len(listaDeCurso)
+                     fecha = datetime.datetime.now()
+                     print("")
+                     print(f"Nombre del usuario: {nombreusuario}          fecha: {fecha}\nNumero de tarjeta: {numeroTarjeta}\nNombre del titular de la tarjeta {nombreTitular}\nEl codigo es: {codigo}\nPara la compra de {cantidad} cursos por el monto de {total}")
+
+                     correcto = int(input("desea confirmar digite 1: "))
+                     if correcto == 1:
+                            print("Compra exitosa, se a enviado la factura a su correo electronico ", a.email)
+
+
+              elif pagos == 2:
+                     print("El precio es ", self.resultado)
+                     print("Usted a elegido tarjeta de debito")
+                     print("")
+                     nombreusuario = a.nombre
+                     numeroTarjeta = int(input("Ingrese el numero de tarjeta por favor"))
+                     nombreTitular = input("Ingrese el nombre que figura en al tarjeta")
+                     codigo = int(input("ingrese el codigo de seguridad que figura al dorso de la tarjeta"))
+                     cantidad = len(listaDeCurso)
+                     fecha = datetime.datetime.now()
+                     print("")
+                     print(f"Nombre del usuario: {nombreusuario}          fecha: {fecha}\nNumero de tarjeta: {numeroTarjeta}\nNombre del titular de la tarjeta {nombreTitular}\nEl codigo es: {codigo}\nPara la compra de {cantidad} cursos por el monto de {self.resultado}")
+
+                     correcto = int(input("desea confirmar digite 1: "))
+                     if correcto == 1:
+                            print("Compra exitosa, se a enviado la factura a su correo electronico ", a.email)
+              else:
+                     print("El precio es ", self.resultado)
+                     print("Usted a elegido tranferencia")
+                     print(f"El alias es CursosOnline, una vez que alla realizado la tranferencia enviarnos el comprobante de pago al email cursosonlines@hotmail.com\nEl impacto del mismo puede demorara 24 hs\nLuego se le enviara un email a {a.email} para confirmar su compra")
